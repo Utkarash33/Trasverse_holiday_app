dad1c0efcf6a64dff51be1665853edc9da71f6a6
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-05-09 00:28:15 +0530

    Slides

--- Script/index.js
+++ Script/index.js
@@ -23,25 +23,74 @@ let all = document.getElementById("all")
  })
 
 // ========================slides======================//
-
-let head = document.querySelector("header")
-let img = new Image()
-let imageLinks = ["Imgs/Slides/1464188297_I2GZl5w7lL-1920x1080.jpg.jpg",
-"Imgs/Slides/1553066319_panchgani_places_feature.jpg.jpg",
-"Imgs/Slides/GettyImages-1008831236-5c65d6bf4cedfd00014aa310.jpg",
-"Imgs/Slides/m_activities_surat_ubharat_umbharat_beach_l_360_640.avif",
-"Imgs/Slides/202664.jpg",
-"Imgs/Slides/pti10032022000206b-1-1157070-1666864723.jpg",
-"Imgs/Slides/morning-rainy-photography-architecture-reflection.jpg",
-"Imgs/Slides/Thar_Khuri.jpg"
-]
-
+let head = document.getElementById("slides");
+let head2 = document.getElementById("slides2");
+head.style.position = "absolute";
+head2.style.position = "absolute";
+head.style.zIndex = -35;
+head2.style.zIndex = -33;
+let img = new Image();
+let imageLinks = [
+  "Imgs/Slides/1464188297_I2GZl5w7lL-1920x1080.jpg.jpg",
+  "Imgs/Slides/1553066319_panchgani_places_feature.jpg.jpg",
+  "Imgs/Slides/GettyImages-1008831236-5c65d6bf4cedfd00014aa310.jpg",
+  "Imgs/Slides/202664.jpg",
+  "Imgs/Slides/Thar_Khuri.jpg",
+  "Imgs/Slides/pti10032022000206b-1-1157070-1666864723.jpg",
+  "Imgs/Slides/morning-rainy-photography-architecture-reflection.jpg"
+];
 let currentIndex = 0;
-head.style.backgroundImage = `url('${imageLinks[currentIndex]}')`;
+let currentImage = new Image();
+currentImage.src = imageLinks[currentIndex];
+head.appendChild(currentImage);
+
 setInterval(() => {
   currentIndex = (currentIndex + 1) % imageLinks.length;
-  head.style.backgroundImage = `url('${imageLinks[currentIndex]}')`;
-}, 3000);
+  let nextImage = new Image();
+  nextImage.src = imageLinks[currentIndex];
+  nextImage.style.opacity = 0;
+  nextImage.style.position = 'absolute';
+  nextImage.style.top = 0;
+  nextImage.style.left = 0;
+  nextImage.style.transition = 'opacity 1s ease-in-out';
+  if (head.style.zIndex == -33) {
+    head.style.zIndex = -35;
+    head2.style.zIndex = -33;
+    head2.innerHTML = null;
+    head2.appendChild(nextImage);
+  } else {
+    head.style.zIndex = -33;
+    head2.style.zIndex = -35;
+    head.innerHTML = null;
+    head.appendChild(nextImage);
+  }
+  setTimeout(() => {
+    currentImage = nextImage;
+    currentImage.style.opacity = 1;
+  }, 50);
+}, 5000);
+
+
+
+// let head = document.querySelector("header")
+// let img = new Image()
+// let imageLinks = ["Imgs/Slides/1464188297_I2GZl5w7lL-1920x1080.jpg.jpg",
+// "Imgs/Slides/1553066319_panchgani_places_feature.jpg.jpg",
+// "Imgs/Slides/GettyImages-1008831236-5c65d6bf4cedfd00014aa310.jpg",
+// "Imgs/Slides/202664.jpg",
+// "Imgs/Slides/Thar_Khuri.jpg",
+// "Imgs/Slides/pti10032022000206b-1-1157070-1666864723.jpg",
+// "Imgs/Slides/morning-rainy-photography-architecture-reflection.jpg"
+// ]
+
+// let currentIndex = 0;
+// head.style.backgroundImage = `url('${imageLinks[currentIndex]}')`;
+// setInterval(() => {
+//   head.style.backgroundImage = null
+//   currentIndex = (currentIndex + 1) % imageLinks.length;
+//   head.style.backgroundImage = `url('${imageLinks[currentIndex]}')`;
+// }, 3000);
+
 
 
 // ==============================Login=======================================//
